2ac8cbd35f27659971f853943bb7a39edd6fb390
aleburrascano <aleburrascano123@gmail.com>
2026-07-27 14:13:55 -0400

    structure: organize code and assets separately

---
+++ server/db/schema/schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE portfolio_manager;
+USE portfolio_manager;
+
+CREATE TABLE Users (
+    userId INTEGER AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(32) NOT NULL,
+    lastName VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE CashTransactions (
+    cashTransactionId INTEGER AUTO_INCREMENT PRIMARY KEY,
+    transactionType ENUM('deposit', 'withdraw') NOT NULL,
+    amount FLOAT NOT NULL,
+    cashTransactionDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    userId INTEGER NOT NULL,
+    FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE
+);
+
+CREATE TABLE StockTransactions (
+    stockTransactionId INTEGER AUTO_INCREMENT PRIMARY KEY,
+    ticker VARCHAR(5) NOT NULL,
+    amount FLOAT NOT NULL,
+    price FLOAT NOT NULL,
+    stockTransactionType ENUM('buy', 'sell') NOT NULL,
+    stockTransactionDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    userId INTEGER NOT NULL,
+    FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE
+);
